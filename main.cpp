1342d4545bf17e8caf093f33ededb158d72df190
LC1B_28_morota <k021g1270@g.neec.ac.jp>
2022-04-11 14:00:26 +0900

    確認課題

--- main.cpp
+++ main.cpp
@@ -18,7 +18,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 	// ゲームウィンドウの作成
 	win = WinApp::GetInstance();
-	win->CreateGameWindow("LE2A_22_モロタ_ソラ_AL3");
+	win->CreateGameWindow("LE2A_22_モロタ_ソラ_AL333");
 
 	// DirectX初期化処理
 	dxCommon = DirectXCommon::GetInstance();
